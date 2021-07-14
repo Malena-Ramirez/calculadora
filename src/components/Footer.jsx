913c8b1ae54f327e9251da2ba1d32866fc34571c
Malena Ramirez <maramirezuribe@gmail.com>
2021-07-14 18:37:12 -0500

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
-  return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  )
-}
+  return <span>&copy; Malena Ramirez Uribe</span>;
+};
 
-export default Footer
+export default Footer;
