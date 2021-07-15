162af469d20860c5f1a1972945bf7606135cb90b
Malena Ramirez <maramirezuribe@gmail.com>
2021-07-15 15:37:33 -0500

    Reorder data

--- src/data.js
+++ src/data.js
@@ -1,51 +1,51 @@
 export const numbers = [
   {
-    id: "decimal",
-    number: "."
-  },
-  {
-    id: "double-zero",
-    number: "00"
-  },
-  {
-    id: "zero",
-    number: "0"
-  },
-  {
-    id: "one",
-    number: "1"
-  },
-  {
-    id: "two",
-    number: "2"
-  },
-  {
-    id: "three",
-    number: "3"
-  },
-  {
-    id: "four",
-    number: "4"
-  },
-  {
-    id: "five",
-    number: "5"
-  },
-  {
-    id: "six",
-    number: "6"
-  },
-  {
-    id: "seven",
-    number: "7"
+    id: "nine",
+    number: "9"
   },
   {
     id: "eight",
     number: "8"
   },
   {
-    id: "nine",
-    number: "9"
+    id: "seven",
+    number: "7"
+  },
+  {
+    id: "six",
+    number: "6"
+  },
+  {
+    id: "five",
+    number: "5"
+  },
+  {
+    id: "four",
+    number: "4"
+  },
+  {
+    id: "three",
+    number: "3"
+  },
+  {
+    id: "two",
+    number: "2"
+  },
+  {
+    id: "one",
+    number: "1"
+  },
+  {
+    id: "zero",
+    number: "0"
+  },
+  {
+    id: "double-zero",
+    number: "00"
+  },
+  {
+    id: "decimal",
+    number: "."
   }
 ];
 
